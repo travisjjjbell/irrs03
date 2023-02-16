d372ee0e744659fba85217538390ca0afc79a02b
travisjjjbell <122818593+travisjjjbell@users.noreply.github.com>
2023-02-16 15:44:43 -0800

    Update Intro to R Course.R

--- Intro to R Course.R
+++ Intro to R Course.R
@@ -1,16 +1,19 @@
 #Tips:
 #ctrl + 1 = move cursor to script
 
-# ctrl + 2 = move cursor to console
+#ctrl + 2 = move cursor to console
 
-# use up and down arrows to cycle through history in console, instead of copy 
+#use up and down arrows to cycle through history in console, instead of copy 
 #and paste 
 
-# use home and end buttons to jump from start of line to end of line and back 
+#use home and end buttons to jump from start of line to end of line and back 
 #on pc, for mac use function + left or right arrows 
 
-# ctrl + L = clear the console, but not actually deleting anything
+#ctrl + L = clear the console, but not actually deleting anything
 
 #Boolean is a variable that takes on two different values: TRUE or FALSE
 
-# (Alt) + (-) = <- 
+#(Alt) + (-) = <- 
+
+#name_things_like_this, not.like.this
+
