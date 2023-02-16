43d0cdd35ae3f89288c150a8c5c36dbd74c84f4e
travisjjjbell <122818593+travisjjjbell@users.noreply.github.com>
2023-02-16 10:31:11 -0800

    Create Intro to R Course.R

---
+++ Intro to R Course.R
@@ -0,0 +1,14 @@
+#Tips:
+#ctrl + 1 = move cursor to script
+
+# ctrl + 2 = move cursor to console
+
+# use up and down arrows to cycle through history in console, instead of copy 
+#and paste 
+
+# use home and end buttons to jump from start of line to end of line and back 
+#on pc, for mac use function + left or right arrows 
+
+# ctrl + L = clear the console, but not actually deleting anything
+
+#Boolean is a variable that takes on two different values: TRUE or FALSE
