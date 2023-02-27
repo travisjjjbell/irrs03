4a35b15b4cd503550f40973a9d0cd10af51b5b7a
Travis Bell <122818593+travisjjjbell@users.noreply.github.com>
2023-02-27 14:39:05 -0800

    Update Stats.R

--- Stats.R
+++ Stats.R
@@ -20,3 +20,62 @@ sigma(m1) # choose any height, get all the people that are this height,
 
 summary(m1)
 
+residuals(m1)
+
+hist(residuals(m1), 50) # check the symmetry of the histogram
+
+summary(m1)$coefficients
+
+confint(m1)
+
+ 
+m2 <- lm(weight ~ height + age, 
+         data = weight_df)
+summary(m2)
+
+hist(residuals(m2), 50)
+
+anova(m1, m2)
+
+m3 <- glm(I(gender == 'Male') ~ height, 
+          data = weight_df,
+          family = binomial)
+summary(m3)
+
+anova(m3, test = 'Chisq') # not including the f-test 
+
+m3_null <- glm(I(gender == 'Male') ~ 1, 
+          data = weight_df,
+          family = binomial)
+
+anova(m3_null, m3, test = 'Chisq' )
+
+m4 <- lm(weight ~ gender, data = weight_df)
+
+
+# oneway anova ------------------------------------------------------------
+
+data('PlantGrowth')
+
+m5 <- lm(weight ~ group,
+         data = PlantGrowth)
+
+anova(m5)
+
+m6 <- aov(weight ~ group,
+          data = PlantGrowth) # aov and lm are one and the same
+summary(m6)
+
+TukeyHSD(m6)
+
+
+
+# twoway anova ------------------------------------------------------------
+
+data("ToothGrowth")
+
+m7 <- lm(len ~ supp * factor(dose), data = ToothGrowth)
+library(car)
+
+Anova(m7)
+Anova(m7, type = 'III')
